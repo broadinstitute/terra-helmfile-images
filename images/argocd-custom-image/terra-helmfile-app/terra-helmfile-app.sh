fd5b28a84ffc189bb699fd1f758a64878c083c3a
Chelsea Hoover <60902147+choover-broad@users.noreply.github.com>
2021-09-28 18:50:31 -0400

    DDO-1572 Adapt terra-helmfile-app plugin to support cluster releases (#23)

--- images/argocd-custom-image/terra-helmfile-app/terra-helmfile-app.sh
+++ images/argocd-custom-image/terra-helmfile-app/terra-helmfile-app.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 #
-# Render manifests for a Terra app in an environment
+# Render manifests for a Terra chart release in a target environment or cluster.
 #
-# This is run by the ArgoCD applications for Terra apps.
+# This script is executed by the ArgoCD applications for Terra apps. (eg. cromwell-dev)
 #
+# TODO: We should make render use viper at some point, it would make these scripts
+# muuuch simpler.
 
 set -eo pipefail
 set -x
 
-if [[ -z "${TERRA_ENV}" ]]; then
-  echo "Usage: Please specify TERRA_ENV as an environment variable" >&2
+target=
+if [[ -n "${TERRA_ENV}" ]]; then
+  target="--env ${TERRA_ENV}"
+elif [[ -n "${TERRA_CLUSTER}" ]]; then
+  target="--cluster ${TERRA_CLUSTER}"
+else
+  echo "Usage: Please specify TERRA_ENV or TERRA_CLUSTER as an environment variable" >&2
   exit 1
 fi
 
-if [[ -z "${TERRA_APP}" ]]; then
-  echo "Usage: Please specify TERRA_APP as an environment variable" >&2
-  exit 1
+if [[ -z "${TERRA_RELEASE}" ]]; then
+  if [[ -z "${TERRA_APP}" ]]; then
+    echo "Usage: Please specify TERRA_RELEASE as an environment variable" >&2
+    exit 1
+  fi
+  # Make it possible to use TERRA_APP to set release name,
+  # for backwards compatibility
+  TERRA_RELEASE="${TERRA_APP}"
 fi
 
 if [[ "$1" == 'init' ]]; then
@@ -33,7 +45,7 @@ elif [[ "$1" == 'generate' ]]; then
   fi
 
   export TERRA_HELMFILE_PATH=$( pwd )
-  render --stdout -e "${TERRA_ENV}" -a "${TERRA_APP}" "${args[@]}"
+  render --stdout $target -r "${TERRA_RELEASE}" "${args[@]}"
 else
   echo "Usage: ${0} (init|generate)" >&2
   exit 1

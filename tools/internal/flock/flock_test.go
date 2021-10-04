12f681a71443133e55e4354715fa553c21e74f14
Chelsea Hoover <chelsea@broadinstitute.org>
2021-10-04 17:51:40 -0400

    Address some flakiness in GHA test runs. Concurrency is hard

--- tools/internal/flock/flock_test.go
+++ tools/internal/flock/flock_test.go
@@ -10,15 +10,17 @@ import (
 	"time"
 )
 
-// Base interval for all sleeps/polling etc in this test.
-const baseInterval = 10 * time.Microsecond
+// Base interval for all sleeps in this test.
+// Unfortunately these concurrency tests depend on timing, and on slow infrastructure,
+// like public GitHub actions runners, long intervals are needed to prevent unexpected
+// failures.
+const baseInterval = 1 * time.Millisecond
 
 const lockRetryInterval = baseInterval
-const lockTimeout = 10_000 * lockRetryInterval
+const lockTimeout = 1000 * baseInterval
 
-// This is the best I could do
 func TestWithLockEnsuresConcurrentExecution(t *testing.T) {
-	numRoutines := 25
+	numWorkers := 25
 
 	// should be small, we want every routine to get the lock in serial
 	// _without_ triggering a timeout
@@ -33,7 +35,7 @@ func TestWithLockEnsuresConcurrentExecution(t *testing.T) {
 	opts := testOptions(t)
 	lockOwner := -1
 
-	for i := 0; i < numRoutines; i++ {
+	for i := 0; i < numWorkers; i++ {
 		id := i // Copy to local variable to prevent leaks
 		go func() {
 			err := WithLock(opts, func() error {
@@ -56,7 +58,7 @@ func TestWithLockEnsuresConcurrentExecution(t *testing.T) {
 	}
 
 	// Verify none of the routines returned an error
-	for i := 0; i < numRoutines; i++ {
+	for i := 0; i < numWorkers; i++ {
 		r := <-ch
 		if r.err != nil {
 			t.Errorf("[%d] unexpected error: %v", r.id, r.err)
@@ -73,18 +75,37 @@ func TestWithLockTimesOut(t *testing.T) {
 	// Launch a goroutine to claim lock in background
 	go func() {
 		errCh <- WithLock(opts, func() error {
+			log.Info().Msgf("[lock thief] obtained the lock")
 			lockedCh <- true
+			log.Info().Msgf("[lock thief] sleeping for %s", lockSleepTime)
 			time.Sleep(lockSleepTime)
 			return nil
 		})
 	}()
 
 	// In foreground, try to get a lock and trigger a timeout
-	<-lockedCh // Block until background routine has lock
+	<- lockedCh // Block until background routine has lock
+	log.Info().Msgf("[foreground] received a signal from thief that it has obtained the lock")
+	start := time.Now()
+	log.Info().Msgf("[foreground] calling withLock...")
+
 	err := WithLock(opts, func() error {
-		t.Fatalf("I should never have obtained the lock!")
+		t.Fatalf("[foreground] I should never have obtained the lock!")
 		return nil
 	})
+
+	actualEndTime := time.Now()
+	actualWaitTime := actualEndTime.Sub(start)
+	expectedEndTime := start.Add(lockTimeout)
+
+	log.Info().Msgf("[foreground] WithLock returned %v after %s", err, actualWaitTime)
+
+	// Allow a delta of 1/8th the existing timeout.
+	// (we shift to avoid casting for a division)
+	// okay - duration is a 64bit unsigned integer.
+	delta := lockTimeout >> 3
+
+	assert.WithinDuration(t, expectedEndTime, actualEndTime, delta, "Expected to get a timeout after about ~%s, got one after %s (allowed delta %s)", lockTimeout, actualWaitTime, delta)
 
 	// Verify we got a timeout error and not something else
 	assert.ErrorIs(t, err, err.(*Error), "Expected an Flock timeout error")
@@ -98,6 +119,7 @@ func TestWithLockReturnsCallbackError(t *testing.T) {
 	err := WithLock(testOptions(t), func() error {
 		return fmt.Errorf("fake error from callback")
 	})
+	assert.Error(t, err, "Expected error to propagate up from WithLock")
 	assert.Equal(t, "fake error from callback", err.Error())
 }
 

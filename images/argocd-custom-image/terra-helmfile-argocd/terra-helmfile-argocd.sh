89245c63ea660e352572f3c40e96e52613f4c431
Chelsea Hoover <chelsea@broadinstitute.org>
2021-12-05 13:54:57 -0500

    bump argo parallel workers to 32

--- images/argocd-custom-image/terra-helmfile-argocd/terra-helmfile-argocd.sh
+++ images/argocd-custom-image/terra-helmfile-argocd/terra-helmfile-argocd.sh
@@ -8,6 +8,7 @@
 set -eo pipefail
 set -x
 
+THELMA_RENDER_PARALLEL_WORKERS=${THELMA_RENDER_PARALLEL_WORKERS:-32}
 
 if [[ "$1" == 'init' ]]; then
   : # Nothing to do
@@ -15,7 +16,7 @@ elif [[ "$1" == 'generate' ]]; then
   # Delegate to `thelma render`
   THELMA_HOME=$( pwd )
   export THELMA_HOME
-  thelma render --stdout --argocd
+  thelma render --stdout --argocd --parallel-workers="${THELMA_RENDER_PARALLEL_WORKERS}"
 else
   echo "Usage: ${0} (init|generate)" >&2
   exit 1

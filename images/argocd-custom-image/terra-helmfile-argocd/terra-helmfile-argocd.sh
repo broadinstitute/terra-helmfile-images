5b43b46afeb347bd621ed9408b58fe6b9b543cec
Chelsea Hoover <chelsea@broadinstitute.org>
2021-12-05 14:15:05 -0500

    DDO-1591 image fix

--- images/argocd-custom-image/terra-helmfile-argocd/terra-helmfile-argocd.sh
+++ images/argocd-custom-image/terra-helmfile-argocd/terra-helmfile-argocd.sh
@@ -8,15 +8,13 @@
 set -eo pipefail
 set -x
 
-THELMA_RENDER_PARALLEL_WORKERS=${THELMA_RENDER_PARALLEL_WORKERS:-32}
-
 if [[ "$1" == 'init' ]]; then
   : # Nothing to do
 elif [[ "$1" == 'generate' ]]; then
   # Delegate to `thelma render`
   THELMA_HOME=$( pwd )
   export THELMA_HOME
-  thelma render --stdout --argocd --parallel-workers="${THELMA_RENDER_PARALLEL_WORKERS}"
+  thelma render --stdout --argocd
 else
   echo "Usage: ${0} (init|generate)" >&2
   exit 1
